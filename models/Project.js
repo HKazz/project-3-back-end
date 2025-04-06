988aec86c6acb60c8515a06029029974fe9c0d5d
Hasan <hasan_kazerooni@outlook.com>
2025-04-06 18:57:49 +0300

    Created Project and tasks models

---
+++ models/Project.js
@@ -0,0 +1,43 @@
+const {Schema, model} = requires("mongoose")
+
+const projectSchema = new Schema({
+    projectName: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    projectDescription: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    startDate: {
+        type: Date,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+    },
+    status: {
+        type: String,
+        enum: ['Not Started', 'In Progress', 'Completed'],
+        default: 'Not Started',
+    },
+    teamMembers: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+    },
+});
+
+const Project = model("Project", projectSchema)
+
+module.exports = Project
+
